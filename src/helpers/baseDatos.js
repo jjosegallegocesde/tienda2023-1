3592a1d5eccf695aa3ba8a3298a7ad50ef607fc4
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-02-22 08:47:55 -0500

    se crean datos d eproductos

---
+++ src/helpers/baseDatos.js
@@ -0,0 +1,39 @@
+//quemar una base de datos
+
+export let productos=Array(
+    {
+        nombre:"Producto 1",
+        descripcion:"Aqui ponga descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/freezer1.webp?alt=media&token=7b87d57d-8a2f-4977-ae69-a7f3f1169e14","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto 2",
+        descripcion:"Aqui ponga descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/gohan1.webp?alt=media&token=ce45c3f8-146b-41c9-81cc-08441e6ed6b4","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto 3",
+        descripcion:"Aqui ponga descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/camizama1.webp?alt=media&token=f257df94-685b-4334-a4fe-05e1ac58d9d9","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto 4",
+        descripcion:"Aqui ponga descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/mandalorian.webp?alt=media&token=f777432d-d98f-421d-8fb9-1134a176d404","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto 5",
+        descripcion:"Aqui ponga descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/mando1.webp?alt=media&token=e3063421-3eb0-4bb6-a39e-6f11f9568440","na2"],
+        precio:70000,
+        popularidad:4.2
+    }
+)
