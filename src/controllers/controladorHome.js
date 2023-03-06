4a66264229c287e3d8fab6551d0239d1e6023d7a
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2023-03-06 09:51:33 -0500

    ok

--- src/controllers/controladorHome.js
+++ src/controllers/controladorHome.js
@@ -2,5 +2,8 @@ import {productos} from '../helpers/baseDatos.js'
 import {pintarProductos} from '../helpers/pintarProductos.js'
 
 
+//llamando a la fila
+let fila=document.getElementById("fila")
+
 //Pintar los productos de la tienda
 pintarProductos(productos)
